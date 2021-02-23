1a8c11bcd31c86d0feb2a7cd0abfbd5891dd1977
Elena <elen.shurigina@yandex.ru>
2021-02-23 22:55:00 +0300

    added login window

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Elenasshu on 05.01.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var login: String?
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let login = self.login else { return }
+        label.text = "Hello, \(login)"
+    }
+    @IBAction func goBackTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "unwindSegue", sender: nil)
+    }
+}
